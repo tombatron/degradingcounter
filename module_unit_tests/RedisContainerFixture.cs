61a0d42aaa1bf098b98b894fe52655bb56a6bef7
Tom Hanks <tommy.hanks@gmail.com>
2024-12-14 09:03:40 -0500

    However ugly it is, it seems to be working.

--- module_unit_tests/RedisContainerFixture.cs
+++ module_unit_tests/RedisContainerFixture.cs
@@ -1,21 +1,53 @@
 using StackExchange.Redis;
 using DotNet.Testcontainers.Builders;
 using Testcontainers.Redis;
+using DotNet.Testcontainers.Images;
+using DotNet.Testcontainers.Volumes;
+using System.Text;
 
 namespace module_unit_tests;
 
 public class RedisContainerFixture : IAsyncLifetime
 {
+    public static string ValgrindLogPath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "valgrind-logs");
+    private readonly IFutureDockerImage _baseImage;
     public RedisContainer RedisContainer { get; }
-    public ConnectionMultiplexer? Redis { get; private set; } 
+    public ConnectionMultiplexer? Redis { get; private set; }
+
+    private IVolume _logVolume;
 
     public RedisContainerFixture()
     {
+        if (Directory.Exists(ValgrindLogPath))
+        {
+            Directory.Delete(ValgrindLogPath, true);
+        }
+
+        Directory.CreateDirectory(ValgrindLogPath);
+
+        _logVolume = new VolumeBuilder().WithName($"valgrind-logs-{Guid.NewGuid():n}").Build();
+        _logVolume.CreateAsync().Wait();
+
+        _baseImage = new ImageFromDockerfileBuilder()
+            .WithDockerfile("Dockerfile")
+            .WithDockerfileDirectory(AppContext.BaseDirectory)
+            .Build();
+
         RedisContainer = new RedisBuilder()
-            .WithImage("redis:7.0")
+            .WithImage(_baseImage)
             .WithName("degrading_counter_tests")
             .WithBindMount(AppContext.BaseDirectory, "/module_unit_tests")
-            .WithCommand("redis-server", "--loadmodule", "/module_unit_tests/degrading-counter.so")
+            .WithVolumeMount(_logVolume.Name, "/valgrind-logs/")
+            .WithCommand(
+                "valgrind",
+                "--leak-check=full",
+                "--track-origins=yes",
+                "--log-file=/valgrind-logs/valgrind.log",
+                "redis-server",
+                "--loadmodule", "/module_unit_tests/degrading-counter.so",
+                "--appendonly", "no",
+                "--save", "''"
+            )
             .WithWaitStrategy(Wait.ForUnixContainer()
                 .UntilCommandIsCompleted("redis-cli", "PING")
             )
@@ -24,6 +56,8 @@ public class RedisContainerFixture : IAsyncLifetime
 
     public async Task InitializeAsync()
     {
+        await _baseImage.CreateAsync();
+
         await RedisContainer.StartAsync();
 
         Redis = await ConnectionMultiplexer.ConnectAsync(RedisContainer.GetConnectionString());
@@ -37,8 +71,37 @@ public class RedisContainerFixture : IAsyncLifetime
             await Redis.CloseAsync();
             await Redis.DisposeAsync();
         }
-        
+
         await RedisContainer.StopAsync();
         await RedisContainer.DisposeAsync();
+
+        try
+        {
+            var container = new ContainerBuilder()
+                .WithImage("busybox")
+                .WithVolumeMount(_logVolume.Name, "/valgrind-logs")
+                .WithName("valgrind-output")
+                .Build();
+
+            try
+            {
+                await container.StartAsync();
+
+                var valgrindLogText = Encoding.UTF8.GetString(await container.ReadFileAsync("/valgrind-logs/valgrind.log"));
+
+                File.WriteAllText("valgrind.log", valgrindLogText);
+
+                Console.WriteLine(valgrindLogText);
+            }
+            finally
+            {
+                await container.StopAsync();
+                await container.DisposeAsync();
+            }
+        }
+        finally
+        {
+            await _logVolume.DeleteAsync();
+        }
     }
 }
---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+dotnet test module_unit_tests/module_unit_tests.csproj
+
+chmod 777 /home/tom/projects/degrading-counter/module_unit_tests/bin/Debug/net8.0/valgrind-logs/valgrind.log
+
+cat /home/tom/projects/degrading-counter/module_unit_tests/bin/Debug/net8.0/valgrind-logs/valgrind.log
